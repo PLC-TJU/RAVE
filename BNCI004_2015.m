49e51415cbbb02e5a06139daf660546d84a208cc
Pan Lincong <132982905+PLC-TJU@users.noreply.github.com>
2023-11-05 10:59:57 +0800

    Create BNCI004_2015.m

---
+++ BNCI004_2015.m
@@ -0,0 +1,44 @@
+%% BNCI 001-2014
+function [data1,label1,data2,label2]=BNCI004_2015(ID)
+
+path='I:\dataset\BNCI\004-2015';
+name={'A','C','D','E','F','G','H','J','L'};
+file=[path,'\',name{ID},'.mat'];
+load(file,'data');
+
+%Rest 3s + Task 7s
+timed=[0,10];
+
+% session 1
+X=data{1}.X;
+trial=data{1}.trial;
+fs=data{1}.fs;
+label1=data{1}.y;
+data1=nan(30,(timed(2)-timed(1))*fs,length(trial));
+for t=1:length(trial)
+    data1(:,:,t)=X(trial(t)+timed(1)*fs:trial(t)+timed(2)*fs-1,:)';
+end
+
+% session 2
+X=data{2}.X;
+trial=data{2}.trial;
+fs=data{2}.fs;
+label2=data{2}.y;
+data2=nan(30,(timed(2)-timed(1))*fs,length(trial));
+for t=1:length(trial)
+    data2(:,:,t)=X(trial(t)+timed(1)*fs:trial(t)+timed(2)*fs-1,:)';
+end
+
+%downsampled
+data1=resample(data1,250,fs,'Dimension',2);
+data2=resample(data2,250,fs,'Dimension',2);
+
+%MI(4: HAND MI, 5: FEET MI)
+data1=data1(:,:,label1>3);
+data2=data2(:,:,label2>3);
+label1=label1(label1>3);
+label2=label2(label2>3);
+label1=label1-3;
+label2=label2-3;
+
+end
